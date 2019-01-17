a856771cc5a457da5e80fff86e01aadd3fe50447
yuanzongrui <yuanzr@ngarihealth.com>
2019-01-17 17:16:40 +0800

    -mFirst commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
+        纳里
+      </router-link>
+      <span class="attribution">
+        An interactive learning project from
+        <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
+          >Vue.js</a
+        >. Code &amp; design licensed under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
