4a845496362a6936620793c483a72fb800fe610f
rayyuan90 <rayyuan90@gmail.com>
2019-01-18 17:19:59 +0800

    add vue webpack cofig

--- vue.config.js
+++ vue.config.js
@@ -42,7 +42,163 @@ module.exports = {
   transpileDependencies: [],
 
   // 生产环境 sourceMap
-  productionSourceMap: false,
+  productionSourceMap: false,module.exports = {
+    //默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上，例如 https://www.my-app.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 publicPath 为 /my-app/。
+
+    //这个值也可以被设置为空字符串 ('') 或是相对路径 ('./')，这样所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径，也可以用在类似 Cordova hybrid 应用的文件系统中。
+    baseUrl: process.env.NODE_ENV === "production" ? "//your_url" : "/",
+
+    //当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
+    outputDir: "dist",
+    //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+    assetsDir: "static",
+    //指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+    // indexPath：'index.html',
+  // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而，这也要求 index 的 HTML 是被 Vue CLI 自动生成的。如果你无法使用 Vue CLI 生成的 index HTML，你可以通过将这个选项设为 false 来关闭文件名哈希。
+    filenameHashing: true,
+
+    // When building in multi-pages mode, the webpack config will contain different plugins
+    // (there will be multiple instances of html-webpack-plugin and preload-webpack-plugin).
+    // Make sure to run vue inspect if you are trying to modify the options for those plugins.
+    pages: {
+      index: {
+        // entry for the pages
+        entry: "src/main.js",
+        // the source template
+        template: "public/index.html",
+        // output as dist/index.html
+        filename: "index.html",
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: "首页",
+        // chunks to include on this pages, by default includes
+        // extracted common chunks and vendor chunks.
+        chunks: ["chunk-vendors", "chunk-common", "index"]
+      },
+      // when using the entry-only string format,
+      // template is inferred to be `public/subpage.html`
+      // and falls back to `public/index.html` if not found.
+      // Output filename is inferred to be `subpage.html`.
+      // subpage: ''
+    },
+
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+
+    // 是否使用包含运行时编译器的Vue核心的构建设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
+    runtimeCompiler: true,
+
+    // 默认情况下 babel-loader 忽略其中的所有文件 node_modules
+    transpileDependencies: [],
+
+    // 生产环境 sourceMap
+    productionSourceMap: false,
+
+    // crossorigin:undefined,
+
+    // cors 相关 https://jakearchibald.com/2017/es-modules-in-browsers/#always-cors
+    // corsUseCredentials: false,
+    // webpack 配置，键值对象时会合并配置，为方法时会改写配置
+    // https://cli.vuejs.org/guide/webpack.html#simple-configuration
+
+    configureWebpack: config => {
+     // add here
+     if (process.env.NODE_ENV === 'production') {
+        // 为生产环境修改配置...
+      } else {
+        // 为开发环境修改配置...
+      }
+    },
+
+    // webpack 链接 API，用于生成和修改 webapck 配置
+    // https://github.com/mozilla-neutrino/webpack-chain
+    chainWebpack: config => {
+      // 因为是多页面，所以取消 chunks，每个页面只对应一个单独的 JS / CSS
+      config.optimization.splitChunks({
+        cacheGroups: {}
+      });
+
+      // 'src/lib' 目录下为外部库文件，不参与 eslint 检测
+      config.module
+        .rule("eslint")
+        .exclude.add("/src/components/TheHeader.vue")
+        .end();
+        // GraphQL Loader
+    config.module
+      .rule('graphql')
+      .test(/\.graphql$/)
+      .use('graphql-tag/loader')
+        .loader('graphql-tag/loader')
+        .end();
+
+        const svgRule = config.module.rule('svg')
+
+     // 清除已有的所有 loader。
+     // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
+     svgRule.uses.clear()
+
+     // 添加要替换的 loader
+     svgRule
+       .use('vue-svg-loader')
+         .loader('vue-svg-loader');
+    },
+
+    // 配置高于chainWebpack中关于 css loader 的配置
+    css: {
+      // 是否开启支持 foo.module.css 样式
+      modules: false,
+
+      // 是否使用 css 分离插件 ExtractTextPlugin，采用独立样式文件载入，不采用 <style> 方式内联至 html 文件中
+      extract: true,
+
+      // 是否构建样式地图，false 将提高构建速度
+      sourceMap: false,
+
+      // css预设器配置项
+      loaderOptions: {
+        css: {
+          // options here will be passed to css-loader
+        },
+
+        postcss: {
+          // options here will be passed to postcss-loader
+        }
+      }
+    },
+
+    // All options for webpack-dev-server are supported
+    // https://webpack.js.org/configuration/dev-server/
+    devServer: {
+
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+
+      open: true,
+
+      host: "localhost",
+
+      port: 3000,
+
+      https: false,
+
+      hotOnly: false,
+  //如果你的前端应用和后端 API 服务器没有运行在同一个主机上，你需要在开发环境下将 API 请求代理到 API 服务器。这个问题可以通过 vue.config.js 中的 devServer.proxy 选项来配置。
+      proxy: null
+
+      //  before: app => {}
+    },
+    // 构建时开启多进程处理 babel 编译
+    parallel: require("os").cpus().length > 1,
+
+    // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+    pwa: {},
+
+    // 第三方插件配置
+    pluginOptions: {}
+  };
+
 
   // cors 相关 https://jakearchibald.com/2017/es-modules-in-browsers/#always-cors
   // corsUseCredentials: false,
