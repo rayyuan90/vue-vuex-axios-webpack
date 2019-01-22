b819517673a472668cadf31a35e7848f4b9a6f42
rayyuan90 <rayyuan90@gmail.com>
2019-01-22 17:55:00 +0800

    add check auth  go login

--- src/views/HomeGlobal.vue
+++ src/views/HomeGlobal.vue
@@ -2,10 +2,12 @@
   <div class="home-global"><RwvArticleList type="all" /></div>
 </template>
 <script>
-import RwvArticleList from "@/components/ArticleList";
+import RwvArticleList from '@/components/ArticleList';
+import { checkAuth } from '@/mixins/mixin';
 
 export default {
-  name: "rwv-home-global",
+  name: 'rwv-home-global',
+  mixins: [checkAuth],
   components: {
     RwvArticleList
   }
