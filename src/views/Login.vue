abc3667f7bcfb0f7d897ada001c7baaf01705221
rayyuan90 <rayyuan90@gmail.com>
2019-01-23 09:45:00 +0800

    change chinese words

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">登陆</h1>
           <p class="text-xs-center">
-            <router-link :to="{ name: 'register' }"> 去注册吧！ </router-link>
+            <router-link :to="{ name: 'register' }"> 去注册 </router-link>
           </p>
           <ul v-if="errors" class="error-messages">
             <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,7 +5,7 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">注册</h1>
           <p class="text-xs-center">
-            <router-link :to="{ name: 'login' }"> 去登陆！ </router-link>
+            <router-link :to="{ name: 'login' }"> 去登陆 </router-link>
           </p>
           <ul v-if="errors" class="error-messages">
             <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
