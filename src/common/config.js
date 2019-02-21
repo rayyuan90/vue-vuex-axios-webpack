ff651aea6c36273f28313da6c670041bebd7606e
rayyuan90 <rayyuan90@gmail.com>
2019-02-21 17:55:36 +0800

    change url

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,4 @@
-export const API_URL = "https://conduit.productionready.io/api";
+// export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL =
+  'https://ssltest.ngarihealth.com/ehealth-base-feature1/*.jsonRequest';
 export default API_URL;
