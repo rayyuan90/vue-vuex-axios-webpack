ab9f01ae7c13209d2a4d545c60a06f222e6bcd5c
ray <rayyuan90@gmail.com>
2019-05-14 17:13:42 +0800

    delete important information

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,2 @@
-// export const API_URL = "https://conduit.productionready.io/api";
-export const API_URL =
-  'https://ssltest.ngarihealth.com/ehealth-base-feature1/*.jsonRequest';
+export const API_URL = "https://conduit.productionready.io/api";
 export default API_URL;
